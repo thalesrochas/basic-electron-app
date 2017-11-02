06d0fe38d71290bb3b77e51e4a8ce9144219480f
Thales Rocha <thales_rochasousa@hotmail.com>
2017-11-02 13:56:16 -0300

    Adicionada conexão com o Banco de dados. Query de teste realizada.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const electron = require('electron');
+const mysql = require('mysql');
 const url = require('url');
 const path = require('path');
 
@@ -8,6 +9,35 @@ let mainWindow;
 
 // Listen for app to be ready
 app.on('ready', function() {
+    // Creating connection
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'lista4'
+    });
+
+    // Connecting with database
+    connection.connect(function(err) {
+        if (err) {
+            console.error('Erro na conexão: ' + err.stack);
+            console.log('Encerrando aplicação!');
+            app.exit();
+        }
+        console.log('Conexão bem sucedida!\nConection ID: ' + connection.threadId);
+    });
+
+    // SQL Query
+
+    let cidade = 'Sobral';
+
+    connection.query('SELECT nome_empregado, cidade FROM empregado WHERE cidade = ?', [cidade],
+    function(error, results, fields) {
+        results.forEach(function(data) {
+            console.log('Nome: ' + data.nome_empregado + '\tCidade: ' + data.cidade);
+        });
+    });
+
     // Create new window
     mainWindow = new BrowserWindow({});
     // Load html file into window
